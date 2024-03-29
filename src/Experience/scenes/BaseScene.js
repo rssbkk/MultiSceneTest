e9234de5e95f0cf1258575ae3f8bd1aedc5539e7
Soleet <vincent.weyh@live.fr>
2024-03-29 10:55:44 +0000

    Review Scene position compute

--- src/Experience/scenes/BaseScene.js
+++ src/Experience/scenes/BaseScene.js
@@ -1,47 +1,72 @@
 import Experience from "../Experience";
 
 export default class BaseScene {
-    constructor(_options)
-    {
-        this.experience = new Experience();
-        this.sizes = this.experience.sizes;
-        this.renderer = this.experience.renderer;
+  constructor(_options) {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.renderer = this.experience.renderer;
 
-        this.id = _options.sceneId;
-        this.elem = _options.elem;
+    this.id = _options.sceneId;
+    this.elem = _options.elem;
+  }
 
-    }
+  computeScenePosition() {
+    const rect = this.elem.getBoundingClientRect();
+    const {
+      top,
+      right,
+      bottom: rectBottom,
+      left: rectLeft,
+      width,
+      height,
+    } = rect;
 
-    update()
-    {
-        const rect = this.elem.getBoundingClientRect();
-        const { left, right, top, bottom, width, height } = rect;
+    const isOffscreen =
+      rectBottom < 0 ||
+      top > this.sizes.height ||
+      right < 0 ||
+      rectLeft > this.sizes.width;
 
-        const isOffscreen =
-        bottom < 0 ||
-        top > this.sizes.height ||
-        right < 0 ||
-        left > this.sizes.width;
+    const bottom = this.sizes.height - rectBottom;
 
-        if (!isOffscreen) {
-            const positiveYUpBottom = this.sizes.height - bottom;
+    return {
+      position: {
+        width,
+        height,
+        left: rectLeft,
+        top,
+        bottom,
+        right,
+      },
+      isOffscreen,
+    };
+  }
 
-            this.renderer.instance.setScissor(left, positiveYUpBottom, width, height);
+  update() {
+    const { position, isOffscreen } = this.computeScenePosition();
 
-            this.renderer.instance.setViewport(
-                left,
-                positiveYUpBottom,
-                width,
-                height,
-            );
-            
-            // // this can log each of the renderer, scene and camera
-            // console.log(this.renderer , "from the base Scene");
+    if (isOffscreen) return;
 
-            this.mesh.rotation.x += 0.01
-            this.mesh.rotation.y += 0.05
+    this.renderer.instance.setScissor(
+      position.left,
+      position.bottom,
+      position.width,
+      position.height,
+    );
 
-            this.renderer.instance.render( this.scene, this.camera );
-        }
-    }
+    this.renderer.instance.setViewport(
+      position.left,
+      position.bottom,
+      position.width,
+      position.height,
+    );
+
+    // this can log each of the renderer, scene and camera
+    // console.log(this.renderer , "from the base Scene");
+
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.05;
+
+    this.renderer.instance.render(this.scene, this.camera);
+  }
 }
